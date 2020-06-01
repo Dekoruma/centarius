30cbc1aeec402076059a2541ffd2a30c04e0de7f
natanelia <natanelia7@gmail.com>
2020-06-02 01:19:34 +0700

    fix(break): change the way of passing server state

--- packages/centarius/src/client/getSsrData.js
+++ packages/centarius/src/client/getSsrData.js
@@ -1,8 +1,7 @@
-import { dataId } from '../core/constants';
-
-const getSsrData = (defaultDataId = dataId) =>
+/* eslint-disable no-underscore-dangle */
+const getSsrData = () =>
   typeof window !== 'undefined' && !!document
-    ? JSON.parse(document.getElementById(defaultDataId).textContent)
+    ? window.__CENTARIUS_SERVER_STATE__
     : {};
 
 export default getSsrData;
--- packages/centarius/src/document/CentariusData.js
+++ packages/centarius/src/document/CentariusData.js
@@ -2,15 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import serialize from 'serialize-javascript';
 
-import { dataId } from '../core/constants';
-
-const CentariusData = ({ id, data, ...rest }) => (
+const CentariusData = ({ data, ...rest }) => (
   <script
-    id={id}
     type="application/json"
     // eslint-disable-next-line
     dangerouslySetInnerHTML={{
-      __html: serialize({ ...data }).replace(/<\/script>/g, '%3C/script%3E'),
+      __html: `window.__CENTARIUS_SERVER_STATE__=${serialize(data)}`,
     }}
     {...rest}
   />
@@ -18,12 +15,10 @@ const CentariusData = ({ id, data, ...rest }) => (
 
 CentariusData.propTypes = {
   data: PropTypes.any,
-  id: PropTypes.string,
 };
 
 CentariusData.defaultProps = {
   data: {},
-  id: dataId,
 };
 
 export default CentariusData;
